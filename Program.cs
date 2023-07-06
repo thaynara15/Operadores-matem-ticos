d864370147f94dc985a784f305b4e701a4fa235a
Thaynara <104026459+thaynara15@users.noreply.github.com>
2023-07-06 17:15:51 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConvertendoValores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            int idade;
+            char sexo;
+            double salario;
+
+            Console.Write("Informe o seu nome: ");
+            nome = Console.ReadLine();
+
+            Console.Write("Informe a sua idade: ");
+            idade = Convert.ToInt16(Console.ReadLine());
+
+            Console.Write("Informe seu sexo M ou F: ");
+            sexo = Convert.ToChar(Console.ReadLine());
+
+            Console.Write("Informe o seu salario: ");
+            salario = Convert.ToDouble(Console.ReadLine());
+
+            Console.ReadKey();
+        }
+    }
+}
